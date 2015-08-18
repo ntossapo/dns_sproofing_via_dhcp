1ed5751c87a402bbc27a147524d5afdc237d8476
Tossapon <ntossapo@gmail.com>
2015-08-19 00:51:11 +0700

    add request type

--- dhcp_discovery_catcher.js
+++ dhcp_discovery_catcher.js
@@ -9,13 +9,13 @@ var server = dgram.createSocket('udp4');
 
 var dhcpRequestType = {
     1 : "DHCP Discover message (DHCPDiscover)",
-    1 : "DHCP Offer message (DHCPOffer)",
-    1 : "DHCP Request message (DHCPRequest)",
-    1 : "DHCP Decline message (DHCPDecline)",
-    1 : "DHCP Acknowledgment message (DHCPAck)",
-    1 : "DHCP Negative Acknowledgment message (DHCPNak)",
-    1 : "DHCP Release message (DHCPRelease)",
-    1 : "DHCP Informational message (DHCPInform)",
+    2 : "DHCP Offer message (DHCPOffer)",
+    3 : "DHCP Request message (DHCPRequest)",
+    4 : "DHCP Decline message (DHCPDecline)",
+    5 : "DHCP Acknowledgment message (DHCPAck)",
+    6 : "DHCP Negative Acknowledgment message (DHCPNak)",
+    7 : "DHCP Release message (DHCPRelease)",
+    8 : "DHCP Informational message (DHCPInform)",
 }
 
 server.on('listening', function(){
@@ -27,24 +27,22 @@ server.on('message', function(message, remote){
     console.log("\n");
     console.log("*************************************************************");
     console.log(remote.address + ":" + remote.port);
-    console.log("*************************************************************");
-    console.log(message);
+    ///console.log("*************************************************************");
+    //console.log(message);
     console.log("*************************************************************");
     console.log(message.length);
     console.log("Transaction Id: 0x" + message.toString("hex", 4, 8));
     console.log("Client Mac Address: " + message.toString("hex", 28, 29) + ":" + message.toString("hex", 29, 30) + ":" + message.toString("hex", 30, 31) + ":" + message.toString("hex", 31, 32) + message.toString("hex", 32, 33) + ":" + message.toString("hex", 33, 34));
-    for(var i = 0;i < message.length;i++){
+    for(var i = 100;i < message.length;i++){
         if(message.toString("hex", i, i+1) == "0c"){
             var length = parseInt(message[i+1].toString());
-            console.log(length);
             var hostname = "";
             for(var j = 0;j < length;j++){
-                hostname += message.toString("ascii", i+j+1, i+j+2);
+                hostname += message.toString("ascii", i+j+2, i+j+3);
             }
             console.log("Host Name: " + hostname);
-            break;
-        }else if(message.toSring("hex", i, i+1) == "35"){
-
+        }else if(message.toString("hex", i, i+1) == "35" && message.toString("hex", i+1, i+2) == "01"){
+            console.log("request type: " + dhcpRequestType[parseInt(message[i+2])]);
         }
     }
     console.log("*************************************************************");
