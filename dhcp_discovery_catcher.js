2e5fa503d581c349bb55583ef34bb5a92b6d9d16
Thotsapon Nuanchuay <ntossapo@gmail.com>
2015-08-20 15:10:11 +0700

    a

--- dhcp_discovery_catcher.js
+++ dhcp_discovery_catcher.js
@@ -2,7 +2,7 @@
  * Created by benvo_000 on 18/8/2558.
  */
 var port = 67;
-var host = "192.168.1.101";
+var host = "172.19.74.84";
 
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
@@ -18,6 +18,28 @@ var dhcpRequestType = {
     8 : "DHCP Informational message (DHCPInform)",
 }
 
+var requestItemList = {
+    1: "Subnet Mask",
+    3: "Router",
+    6: "Domain Name Server",
+    7: "Log Server",
+    12: "Hostname",
+    15: "Domain Name",
+    24:	"MTU Timeout",
+    25: "MTU Plateau",
+    26: "MTU Interface",
+    27: "MTU Subnet",
+    28: "Broadcast Address",
+    31: "Router Discovery",
+    32: "Router Request",
+    33: "Static Route",
+    51: "IP Address Lease Time",
+    58: "Renewal Time Value",
+    59: "Rebinding Time Value",
+    119: "Domain Search",
+    252: "Private/Proxy Auto-Discovery"
+};
+
 server.on('listening', function(){
     var address = server.address();
     console.log('DHCP Catcher listening on ' + address.address + ':' + address.port);
@@ -30,21 +52,47 @@ server.on('message', function(message, remote){
     ///console.log("*************************************************************");
     //console.log(message);
     var date = new Date();
+    var checker = {
+        "MessageType" : false,
+        "RequestIp" : false,
+        "Hostname" : false,
+        "RequestOption" : false
+    }
     console.log(date.getHours()+":"+date.getMinutes()+":"+date.getSeconds());
     console.log("*************************************************************");
     console.log(message.length);
     console.log("Transaction Id: 0x" + message.toString("hex", 4, 8));
-    console.log("Client Mac Address: " + message.toString("hex", 28, 29) + ":" + message.toString("hex", 29, 30) + ":" + message.toString("hex", 30, 31) + ":" + message.toString("hex", 31, 32) + message.toString("hex", 32, 33) + ":" + message.toString("hex", 33, 34));
-    for(var i = 100;i < message.length;i++){
-        if(message.toString("hex", i, i+1) == "0c"){
-            var length = parseInt(message[i+1].toString());
-            var hostname = "";
-            for(var j = 0;j < length;j++){
-                hostname += message.toString("ascii", i+j+2, i+j+3);
+    console.log("Client Mac Address: " + message.toString("hex", 28, 29) + ":" + message.toString("hex", 29, 30) + ":" + message.toString("hex", 30, 31) + ":" + message.toString("hex", 31, 32) + ":" + message.toString("hex", 32, 33) + ":" + message.toString("hex", 33, 34));
+    for(var i = 0;i < message.length;i++){
+        if(message.toString("hex", i, i+4) == "63825363"){
+            for(j=i;j<message.length;j++){
+                if(message[j] == 60 && message[j+1] == 12) {
+                    j = j+2;
+                    continue;
+                }
+                if(message.toString("hex", j, j+1) == "35" && checker["MessageType"] == false){
+                    console.log("Request Type : " + dhcpRequestType[message[j+2]]);
+                    checker["MessageType"] = true;
+                }
+                if(message.toString("hex", j, j+1) == "32" && checker["RequestIp"] == false){
+                    console.log("Request For IP : " + message[j+2] + "." + message[j+3] + "." + message[j+4] + "." + message[j+5]);
+                    checker["RequestIp"] = true;
+                }
+                if(message[j-1] != 60 && message.toString("hex", j, j+1) == "0c" && checker["Hostname"] == false){
+                    var length = parseInt(message[j+1]);
+                    console.log("Hostname : " + message.toString("ascii", j+2, j+2+length));
+                    checker["Hostname"] = true;
+                }
+                if(message.toString("hex", j, j+1) == "37" && checker["RequestOption"] == false){
+                    console.log("Request Item");
+                    var length = message[j+1];
+                    for(var k = j; k < j+length; k++){
+                        console.log("\t- " + requestItemList[message[k]]);
+                    }
+                    checker["RequestOption"] = true;
+                }
             }
-            console.log("Host Name: " + hostname);
-        }else if(message.toString("hex", i, i+1) == "35" && message.toString("hex", i+1, i+2) == "01"){
-            console.log("request type: " + dhcpRequestType[parseInt(message[i+2])]);
+            break;
         }
     }
     console.log("*************************************************************");
